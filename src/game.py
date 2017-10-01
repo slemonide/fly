340bce0333866ca022e631a15c9928c44b8670ec
JessicaNgo <jessica.ngo@live.com>
2017-10-01 00:25:42 -0700

    added space fix

--- src/game.py
+++ src/game.py
@@ -43,8 +43,7 @@ class Game:
         pygame.draw.rect(display, pygame.Color(191, 87, 0), pygame.Rect(0, 0, SCREEN_WIDTH, TEXT_HEIGHT + 5))
 
         surface = self.font.render("Time: " + str("%.1f" % self.player.t) + " seconds | " +
-                                   # "x: " + str(self.player.x) + " light seconds | " +
-                                   "x: " + "%.3f"%self.player.x + "light seconds | " +
+                                   "x: " + "%.3f"%self.player.x + " light seconds | " +
                                    "y: " + "%.3f"%self.player.y + " light seconds"
 
                                    , True, (144, 238, 144))
